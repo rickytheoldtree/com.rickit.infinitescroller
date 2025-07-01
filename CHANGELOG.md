37294ca7d31684e20b0128b6fba4dbb2c7ff8ae0
rickytheoldtree <rickyliyiqi@outlook.com>
2025-07-01 14:18:21 +0800

    v1.0.4

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+## [1.0.4] - 2025-07-01
+- enhance jumpToIndex condition for elastic scrolling
+## [1.0.3] - 2025-06-18
+- Enhance snap behavior by invoking OnSnapComplete for the current index
+## [1.0.2] - 2025-05-28
+- refactor InfiniteScroller to use IInfiniteScrollerCellView interface for cell views
+## [1.0.1] - 2025-04-16
+### This is the first release of *com.rickit.infinitescroller*.
+- first commit
---
+++ Runtime/InfiniteScrollerCellView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace RicKit.InfiniteScroller
+{
+    public class InfiniteScrollerCellView : MonoBehaviour, IInfiniteScrollerCellView
+    {
+        public string cellIdentifier;
+        public string CellIdentifier => cellIdentifier;
+        public int CellIndex { get; set; }
+        public int DataIndex { get; set; }
+        public bool Active { get; set; }
+        public GameObject GameObject => gameObject;
+    }
+
+    public interface IInfiniteScrollerCellView
+    {
+        public string CellIdentifier { get; }
+        public int CellIndex { get; set; }
+        public int DataIndex { get; set; }
+        public bool Active { get; set; }
+        public GameObject GameObject { get; }
+        public virtual void RefreshCellView()
+        {
+        }
+    }
+}
